c8eaaa185c91c01acd2f96bf110141dc9643047f
rawfishthelgh <rg970604@naver.com>
2023-04-27 20:54:14 +0900

    test : 웹어플리케이션 테스트

--- src/test/java/racingcar/RacingCarWebApplicationTests.java
+++ src/test/java/racingcar/RacingCarWebApplicationTests.java
@@ -1,13 +1,21 @@
 package racingcar;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RacingCarWebApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private RacingCarWebApplication racingCarWebApplication;
+
+    @Test
+    void contextLoads() {
+        assertThat(racingCarWebApplication).isNotNull();
+    }
 
 }
